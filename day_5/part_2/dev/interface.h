7c28728e5a2d73e481f783e880fd16a55ac0053b
mribeiro <mrisouza@gmail.com>
2022-11-15 21:08:10 -0300

    include solution to part_2 day_5

--- day_5/part_2/dev/interface.h
+++ day_5/part_2/dev/interface.h
@@ -1,2 +1,17 @@
-FILE* open_file(const char* fname);
-void close_file(FILE* pfile);
+#pragma once
+
+typedef long long int ll_int;
+
+typedef struct Point{
+    ll_int x;
+    ll_int y;
+    /* data */
+    ll_int num_lines;
+} point;
+
+ll_int store_points(point arr_pts[], FILE* pfile);
+ll_int get_max_coord(point arr_pts[], char coord, ll_int total_el);
+void create_grid(point grid[], ll_int max_x, ll_int max_y);
+void print_grid(point grid[], ll_int max_x, ll_int max_y);
+void count_lines(point grid[], point arr_pts[], ll_int max_x, ll_int total_el);
+ll_int print_lines_in_grid(point grid[], ll_int max_x, ll_int max_y);
